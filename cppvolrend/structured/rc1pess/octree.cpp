cde57f5047adc131298d86874d9558d3d56fd368
bazze <83212143+bazze23@users.noreply.github.com>
2024-12-03 11:16:28 +0100

    Implement BuildOctree
    Implemented the construction of the octree. It has to be tested before moving on.

--- cppvolrend/structured/rc1pess/octree.cpp
+++ cppvolrend/structured/rc1pess/octree.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <glm/glm.hpp>
 #include <stdlib.h>
+#include <volvis_utils/utils.h>
 
 // Octree
 struct OctreeNode {
@@ -40,3 +41,55 @@ struct AABB {
 		return max - min;
 	}
 };
+
+// Recursively build the octree
+void BuildOctree(OctreeNode* node, vis::StructuredGridVolume* volume, int maxDepth, int currentDepth) {
+	if (currentDepth >= maxDepth || node->isEmpty) {
+		node->isLeaf = true;
+		return;
+	}
+
+	// Subdivide node
+	node->isLeaf = false;
+
+	for (size_t i = 0; i < 8; i++) {
+		// Compute bounds for child node 'i'
+		AABB childBounds = computeChildBounds(node->bounds, i);
+
+		// Create child node
+		node->children[i] = new OctreeNode(childBounds);
+
+		// Check if child node contains any data
+		node->children[i]->isEmpty = checkIfEmpty(volume, childBounds);
+
+		// Recursively build the child node
+		BuildOctree(node->children[i], volume, maxDepth, ++currentDepth);
+	}
+}
+
+// Compute the child bounds based on its index
+AABB computeChildBounds(const AABB& parentBounds, int childIndex) {
+	glm::vec3 childSize = (parentBounds.max - parentBounds.min) * 0.5f;
+	glm::vec3 offset = computeOffset(childIndex, childSize);
+	return AABB(parentBounds.min + offset, parentBounds.min + offset + childSize);
+}
+
+bool checkIfEmpty(vis::StructuredGridVolume* volume, const AABB& bounds) {
+	for (size_t x = 0; x < bounds.max.x; x++) {
+		for (size_t y = 0; y < bounds.max.y; y++) {
+			for (size_t z = 0; z < bounds.max.z; z++) {
+				if (volume->GetNormalizedSample(x, y, z) > 0) return false;
+			}
+		}
+	}
+	return true;
+}
+
+glm::vec3 computeOffset(int childIndex, glm::vec3 size) {
+	// Calculate offsets based on child index, using bit shifting
+	float xOffset = (childIndex & 1) ? size.x * 0.5f : 0.0f;
+	float yOffset = (childIndex & 2) ? size.y * 0.5f : 0.0f;
+	float zOffset = (childIndex & 4) ? size.z * 0.5f : 0.0f;
+
+	return glm::vec3(xOffset, yOffset, zOffset);
+}
